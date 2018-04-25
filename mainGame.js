3083c90ec115752024eedf3383588312041f8a76
rjbowen <rjbowen97@gmail.com>
2018-04-25 02:51:59 -0400

    Added a system for tallying value and suit totals in a player's hand

--- mainGame.js
+++ mainGame.js
@@ -1,5 +1,3 @@
-const suits = ['H','S','C','D'];
-
 function main() {
 	var jeffPlayer = createPlayer("Jeff");
 	console.log(jeffPlayer);
@@ -7,26 +5,31 @@ function main() {
 	// console.log(deck);
 	shuffle(deck);
 	// console.log(deck);
-	drawCard(jeffPlayer, deck);
+	playerDrawCard(jeffPlayer, deck);
+	playerDrawCard(jeffPlayer, deck);
+	playerDrawCard(jeffPlayer, deck);
+	playerDrawCard(jeffPlayer, deck);
+	playerDrawCard(jeffPlayer, deck);
 	console.log(jeffPlayer);
-	returnCard(jeffPlayer, deck);
-	console.log(jeffPlayer);
-		console.log(deck);
+	checkHandRanking(jeffPlayer.cards)
+	playerReturnCard(jeffPlayer, deck);
+
+	// console.log(jeffPlayer);
+	// console.log(deck);
 }
 
 function createPlayer(name) {
 	var newPlayer = {};
 	newPlayer.name=name;
-	newPlayer.hand=[];
+	newPlayer.cards=[];
 	return newPlayer;
 }
 
 function createDeckOfCards(argument) {
 	var deck = [];
-
-	for (var currentSuitIndex = 0; currentSuitIndex < suits.length; currentSuitIndex++) {
-		for (var currentIndex = 0; currentIndex < 13; currentIndex++) {
-			deck.push(createCard(suits[currentSuitIndex], currentIndex));
+	for (var currentSuitIndex = 0; currentSuitIndex < 4; currentSuitIndex++) {
+		for (var currentValue = 1; currentValue < 14; currentValue++) {
+			deck.push(createCard(currentSuitIndex, currentValue));
 		}
 	}
 	return deck;
@@ -39,12 +42,35 @@ function createCard(suit, value) {
 	return card;
 }
 
-function drawCard(player, deck) {
-	player.hand.push(deck.pop());
+function playerDrawCard(player, deck) {
+	player.cards.push(deck.pop());
 }
 
-function returnCard(player, deck) {
-	deck.push(player.hand.pop());
+function playerReturnCard(player, deck) {
+	deck.push(player.cards.pop());
+}
+
+function checkHandRanking(hand) {
+
+	var suitTotals = [];
+	var valueTotals = [];
+
+
+	for (var currentSuitIndex = 0; currentSuitIndex < 4; currentSuitIndex++) {
+		suitTotals.push([currentSuitIndex, 0]);
+	}
+	for (var currentValue = 1; currentValue < 14; currentValue++) {
+		valueTotals.push([currentValue, 0]);
+	}
+
+	for (var currentCardIndex = 0; currentCardIndex < hand.length; currentCardIndex++) {
+		suitTotals[hand[currentCardIndex].suit][1]++;
+		valueTotals[hand[currentCardIndex].value - 1][1]++;
+	}
+
+	console.log(suitTotals);
+	console.log(valueTotals);
+
 }
 
 //Fisher-yates shuffle
