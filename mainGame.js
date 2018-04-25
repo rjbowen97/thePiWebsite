e6a8694899847981eb64819fabc812d03ef0220e
rjbowen <rjbowen97@gmail.com>
2018-04-25 02:19:06 -0400

    Added the draw function and shuffle functions

--- mainGame.js
+++ mainGame.js
@@ -2,24 +2,26 @@ const suits = ['H','S','C','D'];
 
 function main() {
 	var jeffPlayer = createPlayer("Jeff");
-	var deck = createDeckOfCards();
 	console.log(jeffPlayer);
+	var deck = createDeckOfCards();
 	console.log(deck);
-
+	shuffle(deck);
+	console.log(deck);
 }
 
 function createPlayer(name) {
 	var newPlayer = {};
 	newPlayer.name=name;
+	newPlayer.hand=[];
 	return newPlayer;
 }
 
 function createDeckOfCards(argument) {
-	var deck = {};
+	var deck = [];
 
 	for (var currentSuitIndex = 0; currentSuitIndex < suits.length; currentSuitIndex++) {
 		for (var currentIndex = 0; currentIndex < 13; currentIndex++) {
-			deck[currentIndex + currentSuitIndex*13] = createCard(suits[currentSuitIndex], currentIndex);
+			deck.push(createCard(suits[currentSuitIndex], currentIndex));
 		}
 	}
 	return deck;
@@ -30,6 +32,27 @@ function createCard(suit, value) {
 	card.suit = suit;
 	card.value = value;
 	return card;
+}
+
+function drawCard(player, deck) {
+	player.hand.push(deck.pop())
+}
+
+//Fisher-yates shuffle
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 main();
